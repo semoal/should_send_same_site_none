d80a329ed27ab32cd19a66a1269689f78ff42608
Sergio <smoreno@finametrix.com>
2020-09-23 14:49:11 +0200

    Implemented testing and code

--- lib/should_send_same_site_none.rb
+++ lib/should_send_same_site_none.rb
@@ -2,64 +2,73 @@ require "should_send_same_site_none/version"
 
 module ShouldSendSameSiteNone
   class Error < StandardError; end
-  def is_same_site_compatible(user_agent)
-    return has_webkit_bug(user_agent) || drops_unrecognized_same_site(user_agent)
+  def self.is_same_site_compatible(user_agent)
+    return !is_compatible(user_agent.to_s)
   end
 
   private
 
-  def has_webkit_bug(user_agent)
-    return is_ios_version(12, user_agent) || (
-      is_macosx_version(10, 14, user_agent) &&
-      (is_safari(user_agent) || is_mac_embedded_browser(user_agent))
+  def self.is_compatible(user_agent)
+    return (
+      has_webkit_bug(user_agent) || drops_unrecognized_same_site(user_agent)
     )
   end
 
-  def drops_unrecognized_same_site(user_agent)
-    return (is_chromium_based(user_agent) &&
-    is_chromium_version_at_least(51, user_agent) &&
-    !is_chromium_version_at_least(67, user_agent)) ||
-    (is_uc_browser(user_agent) && !is_uc_browser_version_at_least(12, 13, 2, user_agent))
+  def self.has_webkit_bug(user_agent)
+    return (
+      is_ios_version(12, user_agent) ||
+      (is_macosx_version(10, 14, user_agent) &&
+        (is_safari(user_agent) || is_mac_embedded_browser(user_agent)))
+    )
   end
 
-  def is_ios_version(major, user_agent)
+  def self.drops_unrecognized_same_site(user_agent)
+    return (
+      (is_chromium_based(user_agent) &&
+      is_chromium_version_at_least(51, user_agent) &&
+      !is_chromium_version_at_least(67, user_agent)) ||
+      (is_uc_browser(user_agent) && !is_uc_browser_version_at_least(12, 13, 2, user_agent))
+    )
+  end
+
+  def self.is_ios_version(major, user_agent)
     regex = /\(iP.+; CPU .*OS (\d+)[_\d]*.*\) AppleWebKit\//
     # Extract digits from first capturing group.
     extract_regex_match(user_agent, regex, 1) == major.to_s
   end
 
-  def is_macosx_version(major, minor, user_agent)
+  def self.is_macosx_version(major, minor, user_agent)
     regex = /\(Macintosh;.*Mac OS X (\d+)_(\d+)[_\d]*.*\) AppleWebKit\//
     extract_regex_match(user_agent, regex, 1) == major.to_s && extract_regex_match(user_agent, regex, 2) == minor.to_s
   end
 
-  def is_safari(user_agent)
+  def self.is_safari(user_agent)
     safari_regex = /Version\/.* Safari\//
     user_agent.match(safari_regex) != nil && !is_chromium_based(user_agent)
   end
 
-  def is_mac_embedded_browser(user_agent)
+  def self.is_mac_embedded_browser(user_agent)
     regex = /^Mozilla\/[\.\d]+ \(Macintosh;.*Mac OS X [_\d]+\) AppleWebKit\/[\.\d]+ \(KHTML, like Gecko\)$/
     regex_contains(user_agent, regex)
   end
 
-  def is_chromium_based(user_agent)
+  def self.is_chromium_based(user_agent)
     regex = /Chrom(e|ium)/
     regex_contains(user_agent, regex)
   end
 
-  def is_chromium_version_at_least(major, user_agent)
+  def self.is_chromium_version_at_least(major, user_agent)
     regex = /Chrom[^ \/]+\/(\d+)[\.\d]* /
     version = extract_regex_match(user_agent, regex, 1).to_i
     return version >= major
   end
 
-  def is_uc_browser(user_agent)
+  def self.is_uc_browser(user_agent)
     regex = /UCBrowser\//
     regex_contains(user_agent, regex)
   end
 
-  def is_uc_browser_version_at_least(major, minor, build, user_agent)
+  def self.is_uc_browser_version_at_least(major, minor, build, user_agent)
     regex = /UCBrowser\/(\d+)\.(\d+)\.(\d+)[\.\d]* /
     major_version = extract_regex_match(user_agent, regex, 1).to_i
     minor_version = extract_regex_match(user_agent, regex, 2).to_i
@@ -75,8 +84,8 @@ module ShouldSendSameSiteNone
     return build_version >= build
   end
 
-  def extract_regex_match(string_value, regex, offsetIndex)
-    matches = stringValue.match(regex)
+  def self.extract_regex_match(string_value, regex, offsetIndex)
+    matches = string_value.match(regex) unless string_value.nil?
 
     if (matches != nil && matches[offsetIndex] != nil)
       return matches[offsetIndex]
@@ -85,8 +94,8 @@ module ShouldSendSameSiteNone
     return nil
   end
 
-  def regex_contains(string_value, regex)
-    string_value.match(regex)
+  def self.regex_contains(string_value, regex)
+    matches = string_value.match(regex) unless string_value.nil?
     return matches != nil
   end
 end
